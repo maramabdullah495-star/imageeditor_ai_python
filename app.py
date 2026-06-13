4f127566e827453522cdda36735fc47827987038
Maram Abdullah <maramabdullah495@gmail.com>
2026-06-14 00:29:52 +0300

    Refactor app.py for image processing options
    Updated imports and added user interaction for image processing options.

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-from src.image_viewer import *
+from src import image_viewer
+from src import object_detection
+
+
+print("AI Image Processing Project")
+print("1 - View Image")
+print("2 - Object Detection")
+
+choice = input("Choose option: ")
+
+if choice == "1":
+    image_viewer
+
+elif choice == "2":
+    object_detection
+
+else:
+    print("Invalid choice")
