080d271011c66196ed6c3d1abb2433344ad897c2
Maram Abdullah <maramabdullah495@gmail.com>
2026-06-14 00:32:45 +0300

    Refactor object detection function for clarity

--- src/object_detection.py
+++ src/object_detection.py
@@ -1,58 +1,61 @@
-
 import cv2
 
-img = cv2.imread('../images/employee.png')
 
-classNames = []
+def detect_objects():
 
-classFile = '../files/thing.names'
+    img = cv2.imread('../images/employee.png')
 
-with open(classFile, 'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
+    classNames = []
 
+    classFile = '../files/thing.names'
 
-configPath = '../files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = '../files/frozen_inference_graph.pb'
-
-
-net = cv2.dnn_DetectionModel(weightsPath, configPath)
-
-net.setInputSize(320, 230)
-net.setInputScale(1.0 / 127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
+    with open(classFile, 'rt') as f:
+        classNames = f.read().rstrip('\n').split('\n')
 
 
-classIds, confs, bbox = net.detect(
-    img,
-    confThreshold=0.5
-)
+    configPath = '../files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+    weightsPath = '../files/frozen_inference_graph.pb'
 
 
-for classId, confidence, box in zip(
+    net = cv2.dnn_DetectionModel(weightsPath, configPath)
+
+    net.setInputSize(320, 230)
+    net.setInputScale(1.0 / 127.5)
+    net.setInputMean((127.5, 127.5, 127.5))
+    net.setInputSwapRB(True)
+
+
+    classIds, confs, bbox = net.detect(
+        img,
+        confThreshold=0.5
+    )
+
+
+    for classId, confidence, box in zip(
         classIds.flatten(),
         confs.flatten(),
-        bbox):
+        bbox
+    ):
 
-    cv2.rectangle(
-        img,
-        box,
-        color=(0, 255, 0),
-        thickness=3
-    )
+        cv2.rectangle(
+            img,
+            box,
+            color=(0, 255, 0),
+            thickness=3
+        )
 
-    cv2.putText(
-        img,
-        classNames[classId - 1],
-        (box[0] + 10, box[1] + 20),
-        cv2.FONT_HERSHEY_COMPLEX,
-        1,
-        (0, 0, 255),
-        thickness=2
-    )
+        cv2.putText(
+            img,
+            classNames[classId - 1],
+            (box[0] + 10, box[1] + 20),
+            cv2.FONT_HERSHEY_COMPLEX,
+            1,
+            (0, 0, 255),
+            thickness=2
+        )
 
 
-cv2.imshow('Object Detection', img)
+    cv2.imshow('Object Detection', img)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
