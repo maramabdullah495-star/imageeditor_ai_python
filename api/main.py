c6241ca407c5372985236654b749aed52352478a
Maram Abdullah <maramabdullah495@gmail.com>
2026-06-14 00:39:51 +0300

    Add image processing endpoint to FastAPI

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import cv2
+import numpy as np
+
 
 app = FastAPI(
     title="AI Image Processing API"
@@ -9,4 +12,29 @@ app = FastAPI(
 def home():
     return {
         "message": "AI Image Processing API is running"
+    }
+
+
+@app.post("/process-image")
+async def process_image(file: UploadFile = File(...)):
+
+    contents = await file.read()
+
+    np_image = np.frombuffer(
+        contents,
+        np.uint8
+    )
+
+    img = cv2.imdecode(
+        np_image,
+        cv2.IMREAD_COLOR
+    )
+
+    height, width, _ = img.shape
+
+    return {
+        "filename": file.filename,
+        "width": width,
+        "height": height,
+        "status": "image received"
     }
